92c3cc4fb15f2a5c9bd1de468d0b7350c930aed6
roije <roijellingsgaard@gmail.com>
2016-12-06 10:05:24 +0100

    database

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 //var url = 'mongodb://localhost:27017/store';
+//mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2
 
 module.exports = {
-    mongodb : 'mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2'
+    mongodb : 'mongodb://roije:1234@ds127428.mlab.com:27428/roijeheroku_advanced'
 }
 
